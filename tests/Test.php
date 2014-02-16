695a5206b813620ce6e0b6b2367aa226c84fb2bb
Vitalii Stepanenko <mail@vitaliy.in>
2014-02-17 00:50:23 +0200

    New test cases added

--- tests/Test.php
+++ tests/Test.php
@@ -5,6 +5,26 @@ class Mock
     public $name;
 }
 
+class ConstructorMock
+{
+    public $field;
+
+    public function __construct($val)
+    {
+        $this->field = $val;
+    }
+}
+
+class ConstructorMock2
+{
+    public $field;
+
+    public function __construct($val1, $val2, $val3, $val4 = "default")
+    {
+        $this->field = $val1 . $val2 . $val3 . $val4;
+    }
+}
+
 class Test extends PHPUnit_Framework_TestCase
 {
 
@@ -18,18 +38,45 @@ class Test extends PHPUnit_Framework_TestCase
         $this->facade = new Nayjest\Builder\Builder();
     }
 
-    public function test_construct_with_class()
+    public function test_make_with_class_argument()
     {
 
         $instance = $this->facade->make([], 'Mock');
         $this->assertEquals('Mock', get_class($instance));
     }
 
-    public function test_public_properties()
+    public function test_blueprint_public_properties()
     {
 
         $instance = $this->facade->make(['name' => 'John'], 'Mock');
         $this->assertEquals('John', $instance->name);
+    }
+
+    public function test_facade_registering_blueprint()
+    {
+        $this->facade->register([
+            'class' => 'Mock',
+            'defaults' => ['name' => 'defaultValue']
+        ]);
+        $instance1 = $this->facade->make([], 'Mock');
+        $instance2 = $this->facade->make(['name' => 'Robert'], 'Mock');
+        $this->assertEquals('defaultValue', $instance1->name);
+        $this->assertEquals('Robert', $instance2->name);
+
+    }
+    public function test_constructor_arguments()
+    {
+        $this->facade->register([
+            'class' => 'ConstructorMock',
+            'asConstructorArguments' => ['test_argument1'],
+            'defaults' => ['test_argument1' => 'value1']
+        ]);
+
+        $instance1 = $this->facade->make([], 'ConstructorMock');
+        $instance2 = $this->facade->make(['test_argument1' => 'value2'], 'ConstructorMock');
+
+        $this->assertEquals('value1', $instance1->field);
+        $this->assertEquals('value2', $instance2->field);
     }
 
 } 
