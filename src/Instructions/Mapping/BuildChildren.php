4096e0b8b404ce38a3789d4f85561e463d866c36
Vitaliy Stepanenko <mail@vitaliy.in>
2015-02-25 18:57:27 +0200

    build children mapping added

---
+++ src/Instructions/Mapping/BuildChildren.php
@@ -0,0 +1,30 @@
+<?php
+namespace Nayjest\Builder\Instructions\Mapping;
+use Nayjest\Builder\Blueprint;
+use Nayjest\Builder\Builder;
+use Nayjest\Builder\Instructions\Base\Instruction;
+use Nayjest\Builder\Instructions\Mapping\Base\Mapping;
+use Nayjest\Builder\Scaffold;
+
+class BuildChildren extends Mapping
+{
+    protected $phase = Instruction::PHASE_PRE_INST;
+
+    protected $is_exclusive = true;
+
+    public function __construct($input_name, Blueprint $blueprint)
+    {
+        $this->input_name = $input_name;
+        $this->blueprint = $blueprint;
+    }
+
+    public function applyInternal($value, Scaffold $scaffold)
+    {
+        $builder = new Builder($this->blueprint);
+        $res = [];
+        foreach ($value as $key => $cfg) {
+            $res[$key] = $builder->build($cfg);
+        }
+        $scaffold->input[$this->input_name] = $res;
+    }
+}
