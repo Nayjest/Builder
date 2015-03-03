eb592ae03eab07f8356313500c4796e8b15c5431
Vitaliy Stepanenko <mail@vitaliy.in>
2015-03-03 20:50:38 +0200

    php_doc added

--- src/Instructions/SimpleValueAsField.php
+++ src/Instructions/SimpleValueAsField.php
@@ -4,13 +4,27 @@ namespace Nayjest\Builder\Instructions;
 use Nayjest\Builder\Instructions\Base\Instruction;
 use Nayjest\Builder\Scaffold;
 
+/**
+ * Class SimpleValueAsField
+ *
+ * This class is a build instruction that can be used in class blueprints.
+ *
+ * Instruction converts custom value passed to input
+ * into input array with mentioned value in corresponding field.
+ *
+ * Values that are instances of target class is ignored
+ *
+ * @package Nayjest\Builder\Instructions
+ */
 class SimpleValueAsField extends Instruction
 {
     protected $phase = self::PHASE_PRE_INST;
 
     protected $field_name;
 
-
+    /**
+     * @param string $field_name Field where value will be placed
+     */
     public function __construct($field_name)
     {
         $this->field_name = $field_name;
@@ -26,5 +40,4 @@ class SimpleValueAsField extends Instruction
             }
         }
     }
-
 }
