f035f0390c85f021e2a78af81579180bd9485880
Vitaliy Stepanenko <mail@vitaliy.in>
2015-03-25 12:31:26 +0200

    phpDoc added

--- src/Instructions/Mapping/Base/Mapping.php
+++ src/Instructions/Mapping/Base/Mapping.php
@@ -4,26 +4,60 @@ namespace Nayjest\Builder\Instructions\Mapping\Base;
 use Nayjest\Builder\Instructions\Base\Instruction;
 use Nayjest\Builder\Scaffold;
 
+/**
+ * Class Mapping
+ *
+ * Base class for mapping build instructions
+ *
+ * @package Nayjest\Builder\Instructions\Mapping\Base
+ */
 abstract class Mapping extends Instruction implements MappingInterface
 {
 
+    /**
+     * @var string
+     */
     protected $input_name;
 
-    /** @var bool Field will be removed from input true */
+    /**
+     * If mapping is exclusive, processed value will be removed from input array
+     * @var bool
+     */
     protected $is_exclusive = true;
 
     protected $default;
 
+    /**
+     * Returns true if mapping instruction is designed for specified field
+     *
+     * @param string $input_name
+     * @return bool
+     */
     public function isTargetInput($input_name)
     {
         return $input_name === $this->input_name;
     }
 
+    /**
+     * If mapping is exclusive, processed value will be removed from input array.
+     *
+     * @return bool
+     */
     public function isExclusive()
     {
         return $this->is_exclusive;
     }
 
+    /**
+     * Applies instruction to Scaffold.
+     *
+     * This method will be executed if input contains target value and it's not null
+     * or there is specified default value.
+     *
+     * @param $value
+     * @param Scaffold $scaffold
+     * @return mixed
+     */
     abstract protected function applyInternal($value, Scaffold $scaffold);
 
     public function apply(Scaffold $scaffold)
--- src/Instructions/Mapping/Base/MappingInterface.php
+++ src/Instructions/Mapping/Base/MappingInterface.php
@@ -3,6 +3,13 @@ namespace Nayjest\Builder\Instructions\Mapping\Base;
 
 use Nayjest\Builder\Instructions\Base\InstructionInterface;
 
+/**
+ * Interface MappingInterface
+ *
+ * Interface of mapping build instructions.
+ *
+ * @package Nayjest\Builder\Instructions\Mapping\Base
+ */
 interface MappingInterface extends InstructionInterface
 {
     /**
@@ -12,6 +19,8 @@ interface MappingInterface extends InstructionInterface
     public function isTargetInput($input_name);
 
     /**
+     * If mapping is exclusive, processed value will be removed from input array.
+     *
      * @return bool
      */
     public function isExclusive();
